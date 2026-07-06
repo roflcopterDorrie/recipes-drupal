32fb84216493250af81173360998cf4a23c2fc9b
Dylan Rolfe <mr.dylan.rolfe@gmail.com>
2026-07-06 12:36:22 +1000

    Updated tests to include the whole shopping list workflow

--- playwright/tests/recipe_user.spec.ts
+++ playwright/tests/recipe_user.spec.ts
@@ -1,55 +1,83 @@
 import { test } from '../authenticated_fixtures';
 import { expect } from '@playwright/test';
 
-test('Add a recipe to my list', async ({ page }) => {
-  // Browse recipes.
-  await page.goto('/recipes/indian-pumpkin-curry');
+test.describe.serial('Shopping list flow', () => {
 
-  // User should see the recipe.
-  await expect(page.locator('h1')).toContainText('Indian Pumpkin Curry')
+  test('Add a recipe to my list', async ({ page }) => {
+    // Clear my list
+    await page.goto('/recipes/my-list/clear');
 
-  // Click the add to list button.
-  await page.locator('a[data-button-id="recipes-add-to-list-button"]').click();
+    // Browse recipes.
+    await page.goto('/recipes/indian-pumpkin-curry');
 
-  // Status message.
-  await expect(page.getByLabel('Status message')).toContainText('Status message Recipe added.');
+    // User should see the recipe.
+    await expect(page.locator('h1')).toContainText('Indian Pumpkin Curry')
 
-  await expect(page).toHaveScreenshot();
+    // Click the add to list button.
+    await page.locator('a[data-button-id="recipes-add-to-list-button"]').click();
+
+    // Status message.
+    await expect(page.getByLabel('Status message')).toContainText('Status message Recipe added.');
+
+    await expect(page).toHaveScreenshot();
+  });
+
+  test('Create a shopping list', async ({ page }) => {
+    // Go to my list.
+    await page.goto('/recipes/my-list');
+
+    // Recipe should be available.
+    await expect(page.getByRole('link', { name: 'Indian Pumpkin Curry' })).toBeVisible();
+
+    // Click make shopping list.
+    await page.getByRole('link', { name: 'Make shopping list' }).click();
+
+    // Remove carrot.
+    await page.locator('label').filter({ hasText: '- carrot - 1 cup (150 g) (chopped)' }).click();
+
+    // Generate the shopping list.
+    await page.getByRole('button', { name: 'Make shopping list' }).click();
+
+    // Make sure carrot isn't there.
+    await expect(page.locator('label').filter({ hasText: '- carrot - 1 cup (150 g) (chopped)' })).not.toBeAttached();
+
+    await expect(page).toHaveScreenshot();
+  });
+
+  test('Add custom ingredients to shopping list', async ({ page }) => {
+    // Go to my list.
+    await page.goto('/recipes/shopping-list');
+
+    await page.getByRole('textbox', { name: 'Extra' }).click();
+    await page.getByRole('textbox', { name: 'Extra' }).fill('Tomato sauce\nSoy milk\n\nAvocado');
+    await page.getByRole('button', { name: 'Save' }).click();
+
+    // Make sure Custom list is visible.
+    await expect(page.locator('fieldset[data-drupal-selector="edit-shopping-list-items-custom"]')).toBeVisible();
+
+    // Make sure there are exactly 3 ingredients under custom.
+    await expect(page.locator('fieldset[data-drupal-selector="edit-shopping-list-items-custom"] input')).toHaveCount(3);
+  });
+
+  test('Check off an ingredient', async ({ page }) => {
+
+    
+    // Go to my list.
+    await page.goto('/recipes/shopping-list');
+
+    let tomatoSauceId = await page.locator('label').filter({ hasText: 'Tomato sauce' }).getAttribute('for');
+
+    await page.locator('label').filter({ hasText: 'Tomato sauce' }).click();
+
+    expect(await page.locator('input[data-drupal-selector="' + tomatoSauceId + '"]').isChecked()).toBeTruthy();
+
+    await page.getByRole('button', { name: 'Save' }).click();
+
+    await Promise.all([
+      page.waitForLoadState('load'),
+      expect(await page.locator('input[data-drupal-selector="' + tomatoSauceId + '"]').isChecked()).toBeTruthy()
+    ]);
+  });
 });
-/*
-test('Create a shopping list', async ({ page }) => {
-  // Go to my list.
-  await page.goto('/recipes/my-list');
 
-  // Recipe should be available.
-  await expect(page.getByRole('link', { name: 'Food Indian Pumpkin Curry' })).toBeVisible();
 
-  // Click make shopping list.
-  await page.getByRole('link', { name: 'Make shopping list' }).click();
-
-  // Remove carrot.
-  await page.locator('label').filter({ hasText: '- carrot - 1 cup (150 g) (chopped)' }).click();
-
-  // Generate the shopping list.
-  await page.getByRole('button', { name: 'Make shopping list' }).click();
-
-  // Make sure carrot isn't there.
-  await expect(page.locator('label').filter({ hasText: '- carrot - 1 cup (150 g) (chopped)' })).not.toBeAttached();
-
-  await expect(page).toHaveScreenshot();
-});
-
-test('Add custom ingredients to shopping list', async ({ page }) => {
-  // Go to my list.
-  await page.goto('/recipes/shopping-list');
-
-  await page.getByRole('textbox', { name: 'Extra' }).click();
-  await page.getByRole('textbox', { name: 'Extra' }).fill('Tomato sauce\nSoy milk\n\nAvocado');
-  await page.getByRole('button', { name: 'Save' }).click();
-
-  // Make sure Custom list is visible.
-  await expect(page.locator('fieldset legend').filter({ hasText: 'Custom'})).toBeVisible();
-
-  // Make sure there are exactly 3 ingredients under custom.
-  await expect(page.locator('fieldset legend').filter({ hasText: 'Custom'}).locator('input')).toHaveCount(3);
-});*/
